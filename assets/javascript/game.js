fc95ffcbc6c7f3442b76f8e544de77265066168a
Valentine <abraval@gmail.com>
2019-10-17 23:01:26 -0400

    added a file structure, updated a game.js with working code

---
+++ assets/javascript/game.js
@@ -0,0 +1,97 @@
+//declaring var-s
+
+//array of names
+var names = ["Dobby", "Harry", "Luna ", "Albus", "Sirius", "Ginny", "Voldemort", "Draco"]
+
+//we start the game with o wins and 13 guesses
+var wins = 0;
+var guesses = 10;
+
+//array of letters already guessed
+var lettersGuessed = [];
+
+//let's randomly pick a word
+
+var currentWord = names[Math.floor(Math.random() * names.length)].toLowerCase();
+console.log(currentWord);
+console.log(currentWord.length);
+
+//create a number of underscores based on number of letters
+var under = [];
+
+for (i = 0; i < currentWord.length; i++) {
+    under.push("_");
+}
+console.log(under);
+document.querySelector("#underscores").innerHTML = under;
+document.querySelector("#guessesText").innerHTML = guesses;
+
+// get user input
+document.onkeyup = function (event) {
+    if (guesses < 1) {
+        alert("GAME OVER!")
+        return;
+    }
+    var userInput = event.key;
+    console.log(userInput)
+    if ((userInput >= 'a' && userInput <= 'z') || (userInput >= 'A' && userInput <= 'Z')) {
+    if (currentWord.indexOf(userInput) > -1) {
+        for (var i = 0; i < currentWord.length; i++) {
+            if (userInput === currentWord[i]) {
+                under[i] = userInput;
+                console.log(under);
+            }
+        }
+        document.querySelector("#underscores").innerHTML = under;
+        --guesses;
+        document.querySelector("#guessesText").innerHTML = guesses;
+    } else {
+        lettersGuessed.push(userInput);
+        document.querySelector("#guessedLettersText").innerHTML = lettersGuessed;
+        --guesses;
+        document.querySelector("#guessesText").innerHTML = guesses;
+    }
+}
+
+else {
+  alert("Pick a letter!");
+
+}
+}
+
+
+//pseudo
+
+//check if user input matches one of the letters in current words
+    //if yes
+//*replace corresponding underscore with the correct letter(s)
+//*update guessed letters 
+//*reduce remaining guesses by 1
+    //if no
+//*update guessed letters 
+//*reduce remaining guesses by 1
+    //if user presses a key that not a letter - display error alert
+
+//do that until either:
+
+    //the word is guessed than 
+        //*increase wins by 1 
+        //*display congratulation alert
+        //*display a picture of character
+        //reset the game
+    //user runs out of guesses than
+        //*display Game Over
+        //*reset the game
+        //---------------
+       // .include or .index of
+
+
+
+
+
+
+
+
+
+
+
